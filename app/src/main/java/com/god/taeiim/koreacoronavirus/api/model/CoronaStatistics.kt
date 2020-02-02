6031f6cd8292ffc066cb10b4efa76a0e923c01e3
taeiim <taeiim.dev@gmail.com>
2020-02-03 01:37:25 +0900

    [통계] viewModel data 불러오기, LiveData 세팅

--- app/src/main/java/com/god/taeiim/koreacoronavirus/api/model/CoronaStatistics.kt
+++ app/src/main/java/com/god/taeiim/koreacoronavirus/api/model/CoronaStatistics.kt
@@ -9,6 +9,6 @@ data class CoronaStatistics(
     val koreaSymptomCnt: Int = 0,
     val updateTime: String = "",
     val worldConfirmatorCnt: Int = 0,
-    val worldCoronaImg: String = "http://www.cdc.go.kr/html/a2/namoimage/images/000016/%EC%8B%A0%EC%A2%85%EC%BD%94%EB%A1%9C%EB%82%98_%EC%84%B8%EA%B3%84%EB%B0%9C%EC%83%9D%ED%98%84%ED%99%A9_200202_9%EC%8B%9C%EA%B8%B0%EC%A4%80.jpg",
-    val worldDieCnt: Int = 0
+    val worldDieCnt: Int = 0,
+    val titleMsg: String = "마스크 착용 잊지마세요!"
 )
--- app/src/main/java/com/god/taeiim/koreacoronavirus/ui/statistics/CoronaStatisticsViewModel.kt
+++ app/src/main/java/com/god/taeiim/koreacoronavirus/ui/statistics/CoronaStatisticsViewModel.kt
@@ -1,4 +1,53 @@
 package com.god.taeiim.koreacoronavirus.ui.statistics
 
-class CoronaStatisticsViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.god.taeiim.koreacoronavirus.api.model.CoronaStatistics
+import com.god.taeiim.koreacoronavirus.data.FirebaseRepository
+
+class CoronaStatisticsViewModel(
+    private val firebaseRepository: FirebaseRepository
+) : ViewModel() {
+
+    private val _coronaStatistics = MutableLiveData<CoronaStatistics>()
+    private val _errorFailGetData = MutableLiveData<Throwable>()
+    val errorFailGetData: LiveData<Throwable> get() = _errorFailGetData
+    val coronaStatistics: LiveData<CoronaStatistics> get() = _coronaStatistics
+
+    val coronaCountryCnt = MutableLiveData<Int>()
+    val koreaCancelIsolationCnt = MutableLiveData<Int>()
+    val koreaConfirmatorCnt = MutableLiveData<Int>()
+    val koreaDieCnt = MutableLiveData<Int>()
+    val koreaIsolationCnt = MutableLiveData<Int>()
+    val koreaSymptomCnt = MutableLiveData<Int>()
+    val updateTime = MutableLiveData<String>()
+    val worldConfirmatorCnt = MutableLiveData<Int>()
+    val worldDieCnt = MutableLiveData<Int>()
+    val titleMsg = MutableLiveData<String>()
+
+    fun getStatisticsData() {
+        firebaseRepository.getStatisticsData(success = {
+            _coronaStatistics.value = it
+            setStatisticsLiveData()
+        }, fail = {
+            _errorFailGetData.value = it
+        })
+    }
+
+    private fun setStatisticsLiveData() {
+        coronaStatistics.value?.let {
+            coronaCountryCnt.value = it.coronaCountryCnt
+            koreaCancelIsolationCnt.value = it.koreaCancelIsolationCnt
+            koreaConfirmatorCnt.value = it.koreaConfirmatorCnt
+            koreaDieCnt.value = it.koreaDieCnt
+            koreaIsolationCnt.value = it.koreaIsolationCnt
+            koreaSymptomCnt.value = it.koreaSymptomCnt
+            updateTime.value = "(${it.updateTime} 기준, 출처: 질병관리본부)"
+            worldConfirmatorCnt.value = it.worldConfirmatorCnt
+            worldDieCnt.value = it.worldDieCnt
+            titleMsg.value = it.titleMsg
+        }
+    }
+
 }
