0adf3661e3a86b39ed8b4973cc0f32bb64c0ce8a
taeiim <taeiim.god@gmail.com>
2020-02-03 23:07:32 +0900

    [경로맵] RouteMapViewModel에서 확진자 데이터 가져오기

--- app/src/main/java/com/god/taeiim/koreacoronavirus/ui/routemap/RouteMapViewModel.kt
+++ app/src/main/java/com/god/taeiim/koreacoronavirus/ui/routemap/RouteMapViewModel.kt
@@ -1,4 +1,28 @@
 package com.god.taeiim.koreacoronavirus.ui.routemap
 
-class RouteMapViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.god.taeiim.koreacoronavirus.api.model.Confirmations
+import com.god.taeiim.koreacoronavirus.data.FirebaseRepository
+
+class RouteMapViewModel(
+    private val firebaseRepository: FirebaseRepository
+) : ViewModel() {
+
+    private val _confirmations = MutableLiveData<Confirmations>()
+    private val _errorFailGetConfirmationsData = MutableLiveData<Throwable>()
+    private val confirmations: LiveData<Confirmations> get() = _confirmations
+    private val errorFailGetConfirmationsData: LiveData<Throwable> get() = _errorFailGetConfirmationsData
+
+
+    fun getConfirmationsData() {
+        firebaseRepository.getConfirmationsInfo(success = {
+            _confirmations.value = it
+
+        }, fail = {
+            _errorFailGetConfirmationsData.value = it
+        })
+    }
+
 }
