1149fdfc15cae84d4f829889439941a6337ab6ed
taeiim <taeiim.god@gmail.com>
2020-02-03 19:53:48 +0900

    [뉴스] BindingAdapter에 제목 html text 표시, intentLink 추가

--- app/src/main/java/com/god/taeiim/koreacoronavirus/ui/BindingAdapter.kt
+++ app/src/main/java/com/god/taeiim/koreacoronavirus/ui/BindingAdapter.kt
@@ -1,7 +1,12 @@
 package com.god.taeiim.koreacoronavirus.ui
 
+import android.content.Intent
+import android.net.Uri
+import android.view.View
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import com.god.taeiim.myapplication.extensions.fromHtml
 import java.text.DecimalFormat
 
 @BindingAdapter("textInt")
@@ -9,4 +14,22 @@ fun TextView.textInt(value: Int?) {
     val myFormatter = DecimalFormat("###,###")
     val formattedStringNum = myFormatter.format(value ?: 0)
     this.text = formattedStringNum
+}
+
+@BindingAdapter("setTextHtml")
+fun TextView.setTextHtml(text: String?) {
+    this.text = text?.fromHtml()
+}
+
+@BindingAdapter("intentLinkOnClick")
+fun View.intentLinkOnClick(link: String?) {
+    setOnClickListener {
+        link?.let {
+            ContextCompat.startActivity(
+                context,
+                Intent(Intent.ACTION_VIEW, Uri.parse(it)),
+                null
+            )
+        }
+    }
 }
