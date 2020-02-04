4188b5de979a61d3bb953b38c8d571da151b45ab
taeiim <taeiim.god@gmail.com>
2020-02-04 20:45:43 +0900

    [통계] 개발자소개 다이얼로그 추가

--- app/src/main/java/com/god/taeiim/koreacoronavirus/ui/statistics/CoronaStatisticsFragment.kt
+++ app/src/main/java/com/god/taeiim/koreacoronavirus/ui/statistics/CoronaStatisticsFragment.kt
@@ -1,7 +1,11 @@
 package com.god.taeiim.koreacoronavirus.ui.statistics
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.View
+import android.view.Window
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
@@ -11,6 +15,7 @@ import com.god.taeiim.koreacoronavirus.data.FirebaseRepositoryImpl
 import com.god.taeiim.koreacoronavirus.data.remote.FirebaseRemoteDataSourceImpl
 import com.god.taeiim.koreacoronavirus.databinding.FragmentCoronaStatisticsBinding
 import com.god.taeiim.myapplication.base.BaseFragment
+import com.god.taeiim.myapplication.extensions.linkIntent
 
 class CoronaStatisticsFragment :
     BaseFragment<FragmentCoronaStatisticsBinding>(R.layout.fragment_corona_statistics) {
@@ -33,7 +38,24 @@ class CoronaStatisticsFragment :
 
         vm.setObserves()
         vm.getStatisticsData()
+
+        binding.showInfoDialogBtn.setOnClickListener { showDeveloperInfoDialog() }
     }
+
+    private fun showDeveloperInfoDialog() {
+        val dialog = Dialog(context!!)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_developer_info)
+
+        val infoLayout = dialog.findViewById(R.id.infoDeveloperLayout) as LinearLayout
+        val closeBtn = dialog.findViewById(R.id.closeBtn) as ImageView
+        val facebookLink = "https://www.facebook.com/profile.php?id=100004908165474"
+        infoLayout.setOnClickListener { facebookLink.linkIntent(context!!) }
+        closeBtn.setOnClickListener { dialog.dismiss() }
+
+        dialog.show()
+    }
+
 
     private fun CoronaStatisticsViewModel.setObserves() {
         errorFailGetData.observe(viewLifecycleOwner, Observer { failToGetData() })
