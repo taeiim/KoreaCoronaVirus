cca2d4442e0048a3ed7ff912c28df5db31f20b31
taeiim <taeiim.god@gmail.com>
2020-02-04 15:38:16 +0900

    [경로맵] 확진자 데이터 model default 값 추가

--- app/src/main/java/com/god/taeiim/koreacoronavirus/api/model/Confirmations.kt
+++ app/src/main/java/com/god/taeiim/koreacoronavirus/api/model/Confirmations.kt
@@ -4,22 +4,27 @@ data class Confirmations(
     val confirmations: List<ConirmationInfo>
 ) {
     data class ConirmationInfo(
-        val age: Int?,
-        val confirmDate: String?,
-        val contactPersonCnt: Int?,
-        val cureHospital: String?,
-        val gender: String?,
-        val isShow: Boolean?,
-        val isVisitWuhan: Boolean?,
-        val marker: List<Marker>,
-        val memo: String?,
-        val nationality: String?
+        val age: Int? = 0,
+        val confirmDate: String? = "",
+        val contactPersonCnt: Int? = 0,
+        val cureHospital: String? = "",
+        val gender: String? = "",
+        val isShow: Boolean? = true,
+        val isVisitWuhan: Boolean? = true,
+        val marker: List<Marker>? = null,
+        val memo: String? = "",
+        val nationality: String? = ""
     )
 }
 
 data class Marker(
-    val date: String?,
-    val latitude: Long?,
-    val longitude: String?,
-    val name: String?
+    val date: String? = "",
+    val latitude: String? = null,
+    val longitude: String? = null,
+    val name: String? = ""
+)
+
+data class MarkersInMap(
+    val index: Int,
+    val marker: com.google.android.gms.maps.model.Marker
 )
