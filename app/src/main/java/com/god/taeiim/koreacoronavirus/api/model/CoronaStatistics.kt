bcacdb02de14b8663322012d65007d5edf62230b
taeiim <taeiim.dev@gmail.com>
2020-02-09 19:06:54 +0900

    [통계] 국내완치자, 세계완치자 정보 추가

--- app/src/main/java/com/god/taeiim/koreacoronavirus/api/model/CoronaStatistics.kt
+++ app/src/main/java/com/god/taeiim/koreacoronavirus/api/model/CoronaStatistics.kt
@@ -5,10 +5,12 @@ data class CoronaStatistics(
     val koreaCancelIsolationCnt: Int = 0,
     val koreaConfirmatorCnt: Int = 0,
     val koreaDieCnt: Int = 0,
+    val koreaCuredCnt: Int = 0,
     val koreaIsolationCnt: Int = 0,
     val koreaSymptomCnt: Int = 0,
     val updateTime: String = "",
     val worldConfirmatorCnt: Int = 0,
     val worldDieCnt: Int = 0,
+    val worldCuredCnt: Int = 0,
     val titleMsg: String = "마스크 착용 잊지마세요!"
 )
--- app/src/main/java/com/god/taeiim/koreacoronavirus/ui/statistics/CoronaStatisticsViewModel.kt
+++ app/src/main/java/com/god/taeiim/koreacoronavirus/ui/statistics/CoronaStatisticsViewModel.kt
@@ -19,11 +19,13 @@ class CoronaStatisticsViewModel(
     val koreaCancelIsolationCnt = MutableLiveData<Int>()
     val koreaConfirmatorCnt = MutableLiveData<Int>()
     val koreaDieCnt = MutableLiveData<Int>()
+    val koreaCuredCnt = MutableLiveData<Int>()
     val koreaIsolationCnt = MutableLiveData<Int>()
     val koreaSymptomCnt = MutableLiveData<Int>()
     val updateTime = MutableLiveData<String>()
     val worldConfirmatorCnt = MutableLiveData<Int>()
     val worldDieCnt = MutableLiveData<Int>()
+    val worldCuredCnt = MutableLiveData<Int>()
     val titleMsg = MutableLiveData<String>()
 
     fun getStatisticsData() {
@@ -41,11 +43,13 @@ class CoronaStatisticsViewModel(
             koreaCancelIsolationCnt.value = it.koreaCancelIsolationCnt
             koreaConfirmatorCnt.value = it.koreaConfirmatorCnt
             koreaDieCnt.value = it.koreaDieCnt
+            koreaCuredCnt.value = it.koreaCuredCnt
             koreaIsolationCnt.value = it.koreaIsolationCnt
             koreaSymptomCnt.value = it.koreaSymptomCnt
             updateTime.value = it.updateTime
             worldConfirmatorCnt.value = it.worldConfirmatorCnt
             worldDieCnt.value = it.worldDieCnt
+            worldCuredCnt.value = it.worldCuredCnt
             titleMsg.value = it.titleMsg
         }
     }
